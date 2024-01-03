574afe985f019aa10fa5ada68b4a26cfa58b2c73
Sobebar Ali <sobebar_ali17@hotmail.com>
2024-01-03 13:57:53 +0530

    Config + MongoDB + ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove non-whitelisted properties
+    forbidNonWhitelisted: true, // throw an error if non-whitelisted properties are present
+    transform: true, // transform payload to DTO instance
+  }));
   await app.listen(3000);
 }
 bootstrap();
