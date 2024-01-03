e52c07e927b559a108ba8293c98269d60eac5c40
Sobebar Ali <sobebar_ali17@hotmail.com>
2024-01-03 14:17:21 +0530

    cors + helmet + throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -10,6 +11,23 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // remove non-whitelisted properties
-    forbidNonWhitelisted: true, // throw an error if non-whitelisted properties are present
-    transform: true, // transform payload to DTO instance
-  }));
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove non-whitelisted properties
+      forbidNonWhitelisted: true, // throw an error if non-whitelisted properties are present
+      transform: true, // transform payload to DTO instance
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
